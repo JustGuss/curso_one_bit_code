e355e649ecba091210358cf009eaf5019013d900
jShinki <gustavo.101104@gmail.com>
2023-12-21 15:30:55 -0300

    Avançando na aula 07

--- JavaScript-III/aula-/aula07-praticando-dom/index.js
+++ JavaScript-III/aula-/aula07-praticando-dom/index.js
@@ -1,14 +1,17 @@
 function addContact(){
-    const contactSection = document.querySelector('#contacts-list')
+    const contactSection = document.getElementById('contacts-list');
 
-    const h3 = document.createElement('h3')
-    h3.innerText = 'Contato'
+    const h3 = document.createElement('h3');
+    h3.innerText = 'Contato';
 
-    const ul = document.createElement('ul')
+    const ul = document.createElement('ul');
 
-    const nameLi = document.createElement('li')
-    nameLi.innerText = 'Nome: '
-    const nameInput = document.createElement('input')
-    nameInput.type = 'text'
-    nameInput.name = 'fullname'
+    const nameLi = document.createElement('li');
+    nameLi.innerText = 'Nome: ';
+    const nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.name = 'fullname';
+    nameLi.appendChild(nameInput);
+    ul.appendChild(nameLi);
+
 }
