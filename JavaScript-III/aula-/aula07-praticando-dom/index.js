a49721b44f48939fa9b0294440a55eb7ea0ebdb1
jShinki <gustavo.101104@gmail.com>
2023-12-21 15:15:26 -0300

    Iniciando aula 7 do modulo 3

---
+++ JavaScript-III/aula-/aula07-praticando-dom/index.js
@@ -0,0 +1,14 @@
+function addContact(){
+    const contactSection = document.querySelector('#contacts-list')
+
+    const h3 = document.createElement('h3')
+    h3.innerText = 'Contato'
+
+    const ul = document.createElement('ul')
+
+    const nameLi = document.createElement('li')
+    nameLi.innerText = 'Nome: '
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'fullname'
+}
