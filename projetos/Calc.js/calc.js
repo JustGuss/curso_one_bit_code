e049e819282a8941c9db17f61dc74787ede649d5
jShinki <gustavo.101104@gmail.com>
2024-03-26 21:21:43 -0300

    finalização do projeto guiado calc.js

--- projetos/Calc.js/calc.js
+++ projetos/Calc.js/calc.js
@@ -40,9 +40,12 @@ input.addEventListener('keydown', function (ev) {
 document.querySelector('#equal').addEventListener('click', calculate)
 
 function calculate() {
+    resultInput.value = 'ERROR';
+    resultInput.classList.add('error');
     const result = eval(input.value);
     //o método "eval" avalia o codigo, "roda" o que é passado para ele. Por esta razão é um método que pode gerar uma falha de segurança ja que o usuário pode rodar um codigo javascript pelo metodo.
-    resultInput.value = result
+    resultInput.value = result;
+    resultInput.classList.remove('error');
 }   
 
 document.querySelector('#copyToClipboard').addEventListener('click', function(ev){
