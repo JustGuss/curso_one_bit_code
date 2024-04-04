607b8960d3276802a03b5d7df9f696a26de10c98
jShinki <gustavo.101104@gmail.com>
2024-04-04 17:36:33 -0300

    aula 10 = encadeamento opcional

---
+++ JavaScript-IV/aulas/aula10-encadeamento-opcional/aula10.js
@@ -0,0 +1,28 @@
+const user = {
+    name: "John Doe",
+    email: "doejohn@email.com",
+    friends: [{
+      name: "Mary",
+      address: {
+        street: "Some Street",
+        number: 89
+      }
+    }],
+    age: 42,
+    phone: {
+      countryCode: "+55",
+      ddd: "22",
+      number: "998765432"
+    }
+  }
+  
+  // console.log(user.friends[0].phone.ddd)
+  console.log(user.friends[0].phone?.ddd)
+  console.log(user?.brothers?.length)
+  
+  console.log(user.brothers?.[5].name)
+  
+
+
+  //utilizando "?" é como dizer "se essa propriedade não existir não é necessário ler". 
+ 
