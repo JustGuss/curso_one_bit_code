6d025e2acea7ed485b1f79fc22ca8a96edaf84b2
jShinki <gustavo.101104@gmail.com>
2024-04-15 22:35:40 -0300

    Usando o npm pela primeira vez

---
+++ JavaScript-IV/aulas/23-usando-npm/index.js
@@ -0,0 +1,5 @@
+const lodash = require('lodash'); 
+//quando ultilizando npm não é necessário utilizar o caminho completo "C:/users/gusta/appData..."
+
+console.log(lodash.isArray([]));
+console.log(lodash.kebabCase('Gustavo Pinheiro'))
